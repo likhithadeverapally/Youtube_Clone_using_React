6142eb7b1b895df528f323790b6fdd1d3640026b
likhithadeverapally <dilkhitha08@gmail.com>
2024-11-03 00:01:21 +0530

    done with setup

---
+++ youtube_clone/src/Components/Navbar/Navar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = ()=>{
+    return (
+        <nav className='flex-div'>
+            
+        </nav>
+    )
+}
+
+export default Navbar
